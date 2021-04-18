9815892746ac9d562af15ab98078cd360def925b
Harper He <46692854+harper-he@users.noreply.github.com>
2021-04-17 23:17:29 -0400

    Update Medium.sql

--- HackerRank/Medium.sql
+++ HackerRank/Medium.sql
@@ -9,8 +9,15 @@ SELECT CONCAT("There are a total of ",
   ORDER BY COUNT(Occupation), Occupation;
   
  -- Binary Tree Nodes 
+ -- Method 1:
   SELECT N, 
   (CASE WHEN P IS NULL THEN "Root"
    WHEN N IN (SELECT P FROM BST GROUP BY P HAVING COUNT(N)=2) THEN "Inner" 
    ELSE "Leaf" END) AS Type
    FROM BST ORDER BY N
+ -- Method 2:
+  SELECT N, IF(P IS NULL, "Root", 
+               IF((N IN (SELECT P FROM BST GROUP BY P HAVING COUNT(N)=2)), "Inner", "Leaf")) 
+                         AS Type 
+                          FROM BST 
+                         ORDER BY N
