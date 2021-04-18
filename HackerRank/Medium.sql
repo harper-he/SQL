98684b33d75d7c7fb612b109fef7d87c4f09a92a
Harper He <46692854+harper-he@users.noreply.github.com>
2021-04-17 23:45:46 -0400

    Update Medium.sql

--- HackerRank/Medium.sql
+++ HackerRank/Medium.sql
@@ -7,8 +7,8 @@ SELECT CONCAT("There are a total of ",
   FROM OCCUPATIONS 
   GROUP BY Occupation
   ORDER BY COUNT(Occupation), Occupation;
-  
- -- Binary Tree Nodes 
+
+-- Binary Tree Nodes 
  -- Method 1:
   SELECT N, 
   (CASE WHEN P IS NULL THEN "Root"
@@ -21,3 +21,26 @@ SELECT CONCAT("There are a total of ",
                          AS Type 
                           FROM BST 
                          ORDER BY N
+
+-- New Companies
+                        
+SELECT C.company_code, C.founder,
+ LM.LMN, SM.SMN, M.MN, E.EN
+                         FROM Company AS C
+ LEFT JOIN (SELECT company_code,COUNT(DISTINCT lead_manager_code) AS LMN
+            FROM Lead_Manager
+            GROUP BY company_code) LM
+      ON C.company_code=LM.company_code
+  JOIN (SELECT company_code,COUNT(DISTINCT senior_manager_code) AS SMN
+       FROM Senior_Manager
+       GROUP BY company_code) SM
+    ON C.company_code=SM.company_code
+  JOIN (SELECT company_code,COUNT(DISTINCT manager_code) AS MN
+        FROM Manager
+        GROUP BY company_code) M
+     ON C.company_code=M.company_code
+  JOIN (SELECT company_code,COUNT(DISTINCT employee_code) AS EN
+        FROM Employee
+        GROUP BY company_code) E
+    ON C.company_code=E.company_code
+ORDER BY  C.company_code
