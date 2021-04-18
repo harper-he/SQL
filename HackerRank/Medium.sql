6528bc7731a980558383eb2634bf0970514cc405
Harper He <46692854+harper-he@users.noreply.github.com>
2021-04-18 00:40:41 -0400

    Update Medium.sql

--- HackerRank/Medium.sql
+++ HackerRank/Medium.sql
@@ -44,3 +44,19 @@ SELECT C.company_code, C.founder,
         GROUP BY company_code) E
     ON C.company_code=E.company_code
 ORDER BY  C.company_code
+
+-- Top Competitors                         
+SELECT T2.hacker_id, H.name
+     FROM 
+         (SELECT hacker_id, COUNT(hacker_id) AS num
+          FROM
+          (SELECT S.*, D.score AS full_score 
+                  FROM Submissions AS S
+           JOIN Challenges AS C ON S.challenge_id=C.challenge_id
+           JOIN Difficulty AS D ON C.difficulty_level=D.difficulty_level) T
+          WHERE score=full_score
+          GROUP BY hacker_id) T2
+      JOIN Hackers AS H
+      ON T2.hacker_id=H.hacker_id
+      WHERE T2.num>1
+ORDER BY T2.num DESC, T2.hacker_id 
